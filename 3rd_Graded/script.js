be7bd8db38a7159e3075bae6501b2393ff61c4dd
ejmpurisima <b2029ejmpurisima@pshs.edu.ph>
2026-03-31 18:59:18 +0800

    js and css errors

--- 3rd_Graded/script.js
+++ 3rd_Graded/script.js
@@ -1,12 +1,12 @@
-function plotPoint() {
+function plotPoint(x0, y0, x, y) {
     console.log(x0, y0, x, y) // checks the arguments passed to this function
     in1.innerHTML = x0 + " " + y0 + " " + x + " " + y;
     var point = document.createElement('div');  
     point.className = 'point';
-    
+
     /* control where to place the div on the screen using left and bottom of position:absolute */
-    point.styleleft = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
-    point.stylebottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
+    point.style.left = (Number(x) - Number(x0) + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
+    point.style.bottom = (Number(y) - Number(y0) + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
     document.getElementById('coordinatePlane').appendChild(point);
 
     if (x > x0 && y > y0){
